669130d640af4c14129b17a56ab591667de1402b
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-04-22 12:49:42 +0330

    feat:add-handler-for-task-input

--- src/components/add-task.tsx
+++ src/components/add-task.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { Button, TextInput } from "flowbite-react";
 
 export default function AddTask() {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="flex items-center gap-4">
-      <TextInput type="text" />
-      <Button>Add Task</Button>
-    </div>
+    <form className="flex items-center gap-4" onSubmit={handleSubmit}>
+      <TextInput type="text" required />
+      <Button type="submit">Add Task</Button>
+    </form>
   );
 }
