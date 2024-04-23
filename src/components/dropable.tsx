fef48e8ab2503aa0cd61ea935b52ee451184e134
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-04-23 10:18:18 +0330

    chore:add-comments

--- src/components/dropable.tsx
+++ src/components/dropable.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Droppable, DroppableProps } from "react-beautiful-dnd";
 
 // https://medium.com/@dimplevarshney06/react-beautiful-dnd-unable-to-find-draggable-with-id-item-2-9c8cf8d109cd#:~:text=The%20warning%20message%20Unable%20to,is%20managing%20the%20draggable%20elements.
+// this component is written because of use strict mode in react. It had conflict with dnd
 
 export const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
   const [enabled, setEnabled] = useState(false);
